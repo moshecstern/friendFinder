61316a9dc1cee4215f68dd46dd34463b8c71698a
Moshe Stern <moshecstern@gmail.com>
2019-11-20 22:40:37 -0500

    starter files and html webpages done

---
+++ app/data/friends.js
@@ -0,0 +1,9 @@
+// store data of friends
+var tableArray = [
+    {
+        "name": "moshe",
+        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores": [5,1,4,5,3,4,4,5,4,2]
+    }
+];
+module.exports = tableArray;
